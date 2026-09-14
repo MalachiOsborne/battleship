47691d159ef3aef13082b4bf1dd0f8c0dd42ac04
ramicoder <ramidaood@anonymous>
2026-09-14 22:35:24 +0300

    moved dom html to html file, removed start() function and used a page refresh for the play-again button's event listener:)

--- src/app.js
+++ src/app.js
@@ -13,70 +13,30 @@ import {
   randomReceiveAttack,
 } from "./factories.js";
 
-function start() {
-  document.body.innerHTML = `<header>
-      <h1 style="font-family: cursive; font-size: 60px;">Rami's Battleship</h1>
-    </header>
-    <main class="game-container">
-
-      <div class="ship-dock">
-        <h2 style="margin-bottom: 30px; margin-top: 50px;">Place your ships!</h2>
-        <div id="fleet-container" class="fleet">
-
-          <div class="ship" id="ship-5" data-length="5" draggable="true">
-            <div class="ship-box"></div><div class="ship-box"></div><div class="ship-box"></div><div class="ship-box"></div><div class="ship-box"></div>
-          </div>
-
-          <div class="ship" id="ship-4" data-length="4" draggable="true">
-            <div class="ship-box"></div><div class="ship-box"></div><div class="ship-box"></div><div class="ship-box"></div>
-          </div>
-
-          <div class="ship" id="ship-3" data-length="3" draggable="true">
-            <div class="ship-box"></div><div class="ship-box"></div><div class="ship-box"></div>
-          </div>
-
-          <div class="ship" id="ship-2" data-length="2" draggable="true">
-            <div class="ship-box"></div><div class="ship-box"></div>
-          </div>
-
-          <div class="ship" data-length="1" id="ship-1" style="margin-bottom: 30px;" draggable="true">
-            <div class="ship-box"></div>
-          </div>
+/*
+<form id="modes">
+        <div class="mode">
+          <input type="radio" name="user_level" id="easy" checked>
+          <label for="easy">Easy</label>
         </div>
-        <span>When placed on the board, click to rotate</span>
-      </div>
-      <div class="player-side">
-        <div id="player-board" class="board">
-
+        <div class="mode">
+        <input type="radio" name="user_level" id="medium">
+          <label for="medium">Medium</label>
         </div>
-        <h1 class="attacked hidden">Getting Attacked...</h1>
-      </div>
-
-      <div class="computer-side hidden">
-        <div id="computer-board" class="board">
-
+        <div class="mode">
+            <input type="radio" name="user_level" id="hard">
+            <label for="hard">Hard</label>
         </div>
-        <h1 class="attack hidden">Attack!</h1>
-      </div>
-    </main>
-    <div class="starter">
-      <input type="text" id="player-name" placeholder="Enter your name">
-      <span style="margin-bottom: 10px;" id="ageError" class="error-message" role="alert" aria-live="polite"></span>
-      <button id="player-button">Play</button>
-    </div>
-    <div id="game-over-modal" class="hidden">
-      <div class="modal-content">
-        <h1 id="game-over-text"></h1>
-      </div>
-      <button id="play-again">Play Again?</button>
-    </div>`;
+
+      </form>
+ */
+
   const modal = document.getElementById("game-over-modal");
   const modalText = document.getElementById("game-over-text");
   const playAgain = document.getElementById("play-again");
 
-  playAgain.addEventListener("click", () => {
-    start();
-  });
+  playAgain.addEventListener("click", () => location.reload()
+  );
   const ship1 = createShip(1);
   const ship2 = createShip(2);
   const ship3 = createShip(3);
@@ -419,6 +379,4 @@ function start() {
     computerBoard.style.pointerEvents = "none";
     playerBoard.style.pointerEvents = "none";
   }
-}
 
-start();
